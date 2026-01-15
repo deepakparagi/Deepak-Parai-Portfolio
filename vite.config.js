963f9fd71a1d061a6fcec38b53aaa0538371a89b
deepcipherai <deepakparagi03@gmail.com>
2026-01-15 23:12:49 +0530

    Configure for GitHub Pages hosting

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Deepak_Paragi_Portfoilio/',
+  base: '/Deepak-Parai-Portfolio/',
   plugins: [react()],
   test: {
     globals: true,
